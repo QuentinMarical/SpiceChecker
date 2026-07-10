5afdcf97ea831b88a439d4e06b5773cb6beb2419
Quentin Marical <maricalquentin@live.fr>
2026-07-10 18:35:57 +0200

    Update ClipboardExportFormatter.cs

--- SpiceChecker.WinForms/Services/ClipboardExportFormatter.cs
+++ SpiceChecker.WinForms/Services/ClipboardExportFormatter.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using SpiceChecker.Application.Services;
 using SpiceChecker.Domain.Entities;
+using SpiceChecker.Domain.Enums;
 
 namespace SpiceChecker.WinForms.Services;
 
@@ -18,15 +19,17 @@ public sealed class ClipboardExportFormatter : IClipboardExportFormatter
         }
 
         var sb = new StringBuilder();
-        sb.AppendLine("AssetTag\tFabricant\tModele\tSousEtat\tAnomalie");
+        sb.AppendLine("Étiquette\tFabricant\tModèle\tSous-état\tNiveau\tRésultat d'analyse");
 
         foreach (var asset in assets)
         {
+            var niveau = asset.Evaluation?.Niveau.ToString() ?? string.Empty;
             var anomaly = asset.Evaluation?.Message ?? string.Empty;
             sb.Append(asset.AssetTag).Append('\t')
               .Append(asset.Fabricant).Append('\t')
               .Append(asset.Modele).Append('\t')
-              .Append(asset.SousEtat).Append('\t')
+              .Append(asset.SousEtat.Libelle()).Append('\t')
+              .Append(niveau).Append('\t')
               .AppendLine(anomaly);
         }
 
